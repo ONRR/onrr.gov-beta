91bbb83b0cb58eab40d85eeea11151585eb2d61a
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-12-21 14:46:11 -0500

    3066: Formats numeric values

--- database/changelog/src/views/download_federal_sales_v.sql
+++ database/changelog/src/views/download_federal_sales_v.sql
@@ -8,14 +8,14 @@ SELECT calendar_year "Date",
   state_offshore_region "State/Offshore Region",
   revenue_type "Revenue Type",
   commodity "Commodity",
-  sales_volume "Sales Volume",
-  gas_volume "Gas MMBtu Volume",
-  sales_value "Sales Value",
-  royalty_value_prior_to_allowance "Royalty Value Prior to Allowances (RVPA)",
-  transportation_allowance "Transportation Allowances (TA)",
-  processing_allowance "Processing Allowances (PA)",
-  royalty_value_less_allowance "Royalty Value Less Allowances (RVLA)",
-  effective_royalty_rate "Effective Royalty Rate"
+  TRIM(TO_CHAR(sales_volume, '999G999G999G999G999D99')) "Sales Volume",
+  TRIM(TO_CHAR(gas_volume, '999G999G999G999G999D99')) "Gas MMBtu Volume",
+  CONCAT('$', TRIM(TO_CHAR(sales_value, '999G999G999G999G999D99'))) "Sales Value",
+  CONCAT('$', TRIM(TO_CHAR(royalty_value_prior_to_allowance, '999G999G999G999G999D99'))) "Royalty Value Prior to Allowances (RVPA)",
+  REPLACE(TO_CHAR(transportation_allowance, 'MIL999G999G990.99'), ' ', '') "Transportation Allowances (TA)",
+  REPLACE(TO_CHAR(processing_allowance, 'MIL999G999G990.99'), ' ', '') "Processing Allowances (PA)",
+  CONCAT('$', TRIM(TO_CHAR(royalty_value_less_allowance, '999G999G999G999G999D99'))) "Royalty Value Less Allowances (RVLA)",
+  CONCAT(TRIM(TO_CHAR(effective_royalty_rate * 100, '90D90')), '%') "Effective Royalty Rate"
 FROM sales
 ORDER BY commodity,
   calendar_year;
